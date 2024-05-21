b5a33d3d7a390a6bf820e33222b2d66c60a3d132
Ricardo <rickyamas@gmail.com>
2024-05-21 17:58:17 -0300

    update: 224/05/24

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,11 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import environ
-import os
 
-env = environ.Env()
-environ.Env.read_env()  # lê o arquivo .env
+import os
+import environ
 
 
 
@@ -24,14 +22,36 @@ environ.Env.read_env()  # lê o arquivo .env
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
+# Inicializar a leitura do .env
+env = environ.Env(
+    # Defina casting e valores padrões
+    DEBUG=(bool, False)
+)
+
+# Leia o .env
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+# Agora, você pode usar as variáveis de ambiente
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = env('DEBUG')
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
